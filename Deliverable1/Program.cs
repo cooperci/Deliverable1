fd42d48dba1d04a66bb0007a1dc7739fba7b6af8
cooperci <ceci.cooper.4@gmail.com>
2020-09-10 21:11:19 -0400

    initial push

---
+++ Deliverable1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your password: ");
+            string password = Console.ReadLine();
+            if (password.Length >= 7 && password.Length <= 12)
+            {
+                if (password.Contains("!") && password.Any(char.IsUpper) && password.Any(char.IsLower))
+                {
+                    Console.WriteLine("Password is valid and accepted");
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
+
